702f635b2f07aa3ce16a4da47f2ab481ee4d19be
buuuudzik <slawomir.budzynski@gmail.com>
2018-04-21 18:26:18 +0200

    Adding the logic to the sideDrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,10 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from './../NavigationItems/NavigationItems';
-import SideDrawer from '../SideDrawer/SideDrawer';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <SideDrawer />
         <Logo />
         <nav className={classes.DesktopOnly}>
             <NavigationItems />
