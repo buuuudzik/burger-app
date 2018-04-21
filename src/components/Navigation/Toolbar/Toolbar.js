c7cc9ec387c9b9ea6d4058e6d4ce04f5d78b73ff
buuuudzik <slawomir.budzynski@gmail.com>
2018-04-21 17:12:32 +0200

    Responsive Adjustment to SideDrawing

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,13 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from './../NavigationItems/NavigationItems';
+import SideDrawer from '../SideDrawer/SideDrawer';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
+        <SideDrawer />
         <div>LOGO</div>
         <nav>
             <NavigationItems />
