256c56aa04b33f406725ccd06c5033a8881291ee
buuuudzik <slawomir.budzynski@gmail.com>
2018-04-29 01:06:05 +0200

    Show only current user orders in Orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -106,7 +106,8 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         }
         console.log(order)
         this.props.onOrderBurger(order, this.props.token);
@@ -179,7 +180,8 @@ const mapStateToProps = state => {
         ingredients: state.brb.ingredients,
         price: state.brb.price,
         loading: state.ord.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     };
 };
 
